6f376af28cc843a9c143969ced220e7782dee00d
AlexAviles345 <20233tn198@utez.edu.mx>
2025-09-09 17:51:38 -0600

    [ADD] Funciones de calcular area de circulo, triangulo y rectangulo

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,14 @@
 public class Calculadora {
+
+    public double calcularAreaCirculo(double radio){
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    public double calcularAreaTriangulo(double base, double altura){
+        return (base * altura) / 2;
+    }
+
+    public double calcularAreaRectangulo(double base, double altura){
+        return base * altura;
+    }
 }

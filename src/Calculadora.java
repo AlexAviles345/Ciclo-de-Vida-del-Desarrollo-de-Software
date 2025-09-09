e373aa27eac0ac3a5cbc086a110ee0178c17a0aa
LennyRocha <20233tn094@utez.edu.mx>
2025-09-09 17:52:45 -0600

    [ADD] Métodossss

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,18 @@
 public class Calculadora {
+
+    public static double calcularAreaCuadrado(double l) {
+        return l * l;
+    }
+
+    public static double calcularPromedio ( double[] nums ){
+        double total = 0;
+        for (double d : nums) {
+            total += d;
+        }
+        return  total / nums.length;
+    }
+
+    public static double calcularRaizCuadrada ( double n ){
+        return n/n;
+    }
 }
